d2ab040eb36437bfa4d858b0077ee5dec96450d4
kobayashi-tatsu <kobayashi_ta@aid-dcc.com>
2024-04-18 19:13:05 +0900

    jsxにしたら動いたが

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { BabylonScene } from "@/components/BabylonScene";
 import { BabylonScene2 } from "@/components/BabylonScene2";
+import { BabylonScene3 } from "@/components/BabylonScene3";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       {/* <BabylonScene /> */}
-      <BabylonScene2 />
+      {/* <BabylonScene2 /> */}
+      <BabylonScene3 />
     </main>
   );
 }
